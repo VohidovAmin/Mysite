e8643308d38685ac497a4d7279ec6e3604a134b5
VohidovAmin <vohidovaminjon9@gmail.com>
2025-10-21 21:45:20 +0300

    Третий коммит

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 new WOW().init();
+const mySwiper = new Swiper('.projects-slider', {
+    pagination: {
+        el: '.projects-pagination',
+        bulletClass: 'projects-bullet',
+        bulletActiveClass: 'projects-bullet-active',
+        clickable: true
+    }
+})
